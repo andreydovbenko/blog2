ac6e9044f1748e3eeef0b5209e78ad26918c7e6c
Andrey Dovbenko <frantic_ad@mail.ru>
2021-03-04 22:50:59 +0300

    Create category. index page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function(){
- Route::get('/', [App\Http\Controllers\Admin\DashboardController:: class, 'dashboard'])->name('admin.index');
- Route::resource('/category', 'CategoryController', ['use'=>'admin']);
+ Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+ Route::resource('/category', 'CategoryController', ['as'=>'admin']);
 });
 
 Route::get('/', function () {
